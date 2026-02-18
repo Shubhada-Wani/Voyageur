8f0474e95663182fd8eaaa106baf5ba0cb7eed82
Shubhada Wani <wanis9414@gmail.com>
2026-02-18 20:30:13 +0530

    temporary debug is true

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o+4n#ad#3%pke)f!5yp_f@h1qzlv1idgkv#ing3jzen@=x5a)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
